ba1212e19d70da41aae4a1c04ea7b29d6f0c07f5
ticoniq_ <okolietochi@gmail>
2023-09-07 09:07:47 +0100

    updated readme

--- solver.rb
+++ solver.rb
@@ -16,5 +16,15 @@ class Solver
     reverse_str.join
   end
 
-  
+  def fizzbuzz(num)
+    if (num % 3).zero? && (num % 5).zero?
+      'fizzbuzz'
+    elsif (num % 5).zero?
+      'buzz'
+    elsif (num % 3).zero?
+      'fizz'
+    else
+      num.to_s
+    end
+  end
 end
